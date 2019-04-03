709c443cb36eca952b5b39e2b51e2a0b50a56dd3
DanFCo <danfcorreia@gmail.com>
2019-04-03 18:03:36 -0400

    me and sabrina

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,56 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
+
     erb :welcome
   end
+
+get "/artists" do
+  @artists = Artist.all
+  erb :index
+end
+
+get "/artists/new" do
+
+  erb :new
+end
+
+post "/artist" do
+  @artist = Artist.create(params)
+  redirect to "artist/#{@artist.id}"
+end
+
+
+
+get '/artist/:id' do
+  @artist = Artist.find(params[:id])
+  erb :show
+end
+
+
+get "/artist/:id/edit" do
+  @artist = Artist.find_by(params[:id])
+  erb :edit
+end
+
+
+
+patch "/artist/:id" do
+  @artist = Artist.find_by(params[:id])
+  @artist.name = params[:name]
+  @artist.age = params[:age]
+  @artist.bio = params[:bio]
+  @artist.save
+  redirect to "/artists/#{artist.id}"
+
+end
+
+delete "/artist/:id/delete" do
+  @artist = Artist.find(params[:id])
+  @artist.destroy
+  redirect to "/artists"
+end
+
+
 
 end
